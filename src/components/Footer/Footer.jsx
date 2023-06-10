a24ea42d5e986f153b1b8d7ef0e8d1be4d8672b8
David <luisdavidbeslanga@gmail.com>
2023-06-10 18:10:30 -0500

    cambios footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer(){
+    return (
+    <div class="flex justify-center">
+        <div>
+            <img src="src/assets/logo-footer.png" alt="logo footer" />
+        </div>
+        <div>
+        <img src="src/assets/bullets.png" alt="Bullets" />
+        </div>
+    </div>
+    )
+}
+
+export default Footer
