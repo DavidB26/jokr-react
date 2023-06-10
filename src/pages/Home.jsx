70370d0d5fd147ead672f4b5f10927e9dac2165e
Marcelo Lanegra <marcelo3096@gmail.com>
2023-06-10 18:45:33 -0500

    llamando footer y header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 function Home() {
   return (
-    <div>
-      Holaaa
-    </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   )
 }
 
