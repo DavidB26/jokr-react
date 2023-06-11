47d63340114f59815f5c492067424d9723ff7018
David <luisdavidbeslanga@gmail.com>
2023-06-10 20:03:33 -0500

    cambios footer - home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,30 @@ import Header from "../components/Header/Header";
 function Home() {
   return (
     <>
-      <Header/>
-      <Footer/>
+      <Header />
+
+      <div className="bg-light-blue-jokr">
+        <div>
+          <h1 className="text-5xl text-white text-center">
+            Todo lo que necesitas,{" "}
+            <span className="text-green-jokr">en minutos</span>
+          </h1>
+          <p className="text-base text-white">El futuro del supermercado está aquí.
+En minutos te llevamos frutas, verduras, tus marcas favoritas y todo lo que necesitas.</p>
+        </div>
+        <div className="flex justify-around items-center">
+          <div>
+            <img src="src/assets/logo-green.png" alt="logo green jokr" />
+          </div>
+          <div>
+            <h2 className="text-green-jokr text-2xl">Descarga la <br/> App</h2>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        "grey-jokr": "#7D9DB7",
         "dark-blue-jokr": "#032039",
         "light-blue-jokr": "#073F70",
         "green-jokr": "#57E39E",
