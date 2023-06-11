4160bf53604c1de3f36d394221048d877e8d8588
David <luisdavidbeslanga@gmail.com>
2023-06-10 20:35:47 -0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,65 @@ function Home() {
     <>
       <Header />
 
-      <div className="bg-light-blue-jokr">
-        <div>
-          <h1 className="text-5xl text-white text-center">
-            Todo lo que necesitas,{" "}
-            <span className="text-green-jokr">en minutos</span>
-          </h1>
-          <p className="text-base text-white">El futuro del supermercado está aquí.
-En minutos te llevamos frutas, verduras, tus marcas favoritas y todo lo que necesitas.</p>
-        </div>
-        <div className="flex justify-around items-center">
+      <div className="bg-light-blue-jokr p-4">
+        <div className="flex flex-col justify-center">
           <div>
-            <img src="src/assets/logo-green.png" alt="logo green jokr" />
+            <div>
+              <h1 className="text-5xl text-white text-center">
+                Todo lo que necesitas,{" "}
+                <span className="text-green-jokr">en minutos</span>
+              </h1>
+              <p className="py-4  text-base text-white text-center">
+                El futuro del supermercado está aquí. En minutos te llevamos
+                frutas, verduras, tus marcas favoritas y todo lo que necesitas.
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-5">
+              <div>
+                <img
+                  className="w-16"
+                  src="src/assets/logo-green.png"
+                  alt="logo green jokr"
+                />
+              </div>
+              <div>
+                <h2 className="text-green-jokr text-2xl leading-6">
+                  Descarga la <br /> App
+                </h2>
+              </div>
+            </div>
+
+            <div className="py-4 flex justify-center items-center gap-5">
+              <div>
+                <img
+                  src="src/assets/logo-google-play.png"
+                  alt="logo Play Store"
+                />
+              </div>
+
+              <div>
+                <img src="src/assets/logo-app-store.png" alt="Logo App Store" />
+              </div>
+            </div>
           </div>
           <div>
-            <h2 className="text-green-jokr text-2xl">Descarga la <br/> App</h2>
+            <img src="src/assets/banner-groceries.png" alt="" />
           </div>
         </div>
       </div>
+
+      <div className="bg-dark-blue-jokr">
+          <div>
+            <img src="src/assets/cohete.png" alt="Cohete" />
+            <h2 className="text-grey-jokr text-2xl">Envío al instante</h2>
+            <p className="text-green-jokr text-base">El tiempo lo es todo, estaremos en tu puerta más rápido de lo que te imaginas.</p>
+          </div>
+          <div>
+            <img src="src/assets/cohete.png" alt="Cohete" />
+            <h2 className="text-grey-jokr">Envío al instante</h2>
+            <p className="text-green-jokr">El tiempo lo es todo, estaremos en tu puerta más rápido de lo que te imaginas.</p>
+          </div>
+        </div>
 
       <Footer />
     </>
