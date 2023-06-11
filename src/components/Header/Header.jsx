4f56b7d6541766caab00db53baf7e807c05bfee4
Marcelo Lanegra <marcelo3096@gmail.com>
2023-06-10 19:09:35 -0500

    adding colours

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className="flex flex-row h-4 sm:flex-col bg-green-jokr">
+      <div className="">holas</div>
+      <div className="">hola</div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue-jokr": "#032039",
+        "light-blue-jokr": "#073F70",
+        "green-jokr": "#57E39E",
+      },
+    },
   },
   plugins: [],
 };
-
